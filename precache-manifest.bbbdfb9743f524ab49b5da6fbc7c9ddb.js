b54843b2e947a6b2c1e2b1ce715f03215ffef23a
Rachel Wegener <rachel.e.wegener@gmail.com>
2023-02-01 16:25:58 -0500

    Updates

---
+++ precache-manifest.bbbdfb9743f524ab49b5da6fbc7c9ddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af34479e949a12e68172",
+    "url": "/valentines_day2023/static/css/main.e8ee3be0.chunk.css"
+  },
+  {
+    "revision": "af34479e949a12e68172",
+    "url": "/valentines_day2023/static/js/main.af34479e.chunk.js"
+  },
+  {
+    "revision": "f1aa738279730670c7ab",
+    "url": "/valentines_day2023/static/js/1.f1aa7382.chunk.js"
+  },
+  {
+    "revision": "bbbb99c53bb640b021b2",
+    "url": "/valentines_day2023/static/js/2.bbbb99c5.chunk.js"
+  },
+  {
+    "revision": "ea7ee8022961111cdf47",
+    "url": "/valentines_day2023/static/js/runtime~main.ea7ee802.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/valentines_day2023/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "27149dcf50c543505647150fd80c33ca",
+    "url": "/valentines_day2023/index.html"
+  }
+];
